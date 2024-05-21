81e5da3b66ca32bf6778669058878ef71f95d6e2
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2024-05-21 20:18:00 +0300

    chore: add transaction script

--- task.sql
+++ task.sql
@@ -2,10 +2,13 @@
 USE ShopDB; 
 
 -- Some data should be created outside the transaction (here)
+INSERT INTO Orders(CustomerID, DATE) VALUES(1, '2024-05-21');
+SET @OrderID = LAST_INSERT_ID();
 
 -- Start the transaction 
 START TRANSACTION; 
-
+INSERT INTO OrderItems(OrderID, ProductID, Count) VALUES (@OrderID, 1, 1);
+UPDATE Products SET WarehouseAmount = WarehouseAmount - 1 WHERE Products.ID = 1;
 -- And some data should be created inside the transaction 
 
 COMMIT; 
